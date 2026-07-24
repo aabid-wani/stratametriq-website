8bf083f9e25696bedbfc2ef97f9c55389224f009
abidhussain <109906624+aabidhussainwani@users.noreply.github.com>
2026-07-24 16:40:31 +0530

    Configure static export for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  
+  // REQUIRED FOR GITHUB PAGES: Tell Next.js to generate static HTML into an "out" folder
+  output: "export",
+  
+  // REQUIRED FOR GITHUB PAGES: Next.js server-side image optimization doesn't work on static hosts
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
